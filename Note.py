04703f9d61d7e927bc2e7841b7adf4c877e8256c
Yuliya <ju7iab@gmail.com>
2023-05-11 22:43:06 +0300

    defs's names change

--- Note.py
+++ Note.py
@@ -2,14 +2,14 @@ from datetime import datetime
 
 
 class Note:
-    def init(self, id_note, title, body, created_at=None, updated_at=None):
+    def __init__(self, id_note, title, body, created_at=None, updated_at=None):
         self.id = id_note
         self.title = title
         self.body = body
         self.created_at = created_at or datetime.now()
         self.updated_at = updated_at or datetime.now()
 
-    def str(self):
+    def __str__(self):
         return f"Заметка #{self.id}: {self.title}\n{self.body}\nСоздана: {self.created_at}\nИсправлена: {self.updated_at}"
 
     def update(self, title, body):
